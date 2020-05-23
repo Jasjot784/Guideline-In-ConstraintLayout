f515daebba78e82b26eaafbd947fa01266ebdd5d
Jasjot Singh <jasjot784@gmail.com>
2020-05-23 08:14:12 +0530

    Successfully added authentication with otp

---
+++ FIrebaseOtp/app/src/main/java/com/jasjotsingh/firebaseotp/DataActivity.java
@@ -0,0 +1,120 @@
+package com.jasjotsingh.firebaseotp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class DataActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etPhone, etOtp;
+    Button btSendOtp, btResendOtp, btVerifyOtp,btSignOut;
+    String mVerificationId;
+    private FirebaseAuth mAuth;
+    PhoneAuthProvider.ForceResendingToken mResendToken;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+        initFields();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initFireBaseCallbacks();
+    }
+    void initFields(){
+        etPhone = findViewById(R.id.et_phone);
+        etOtp = findViewById(R.id.et_otp);
+        btSendOtp = findViewById(R.id.bt_send_otp);
+        btResendOtp = findViewById(R.id.bt_resend_otp);
+        btVerifyOtp = findViewById(R.id.bt_verify_otp);
+        btSignOut = findViewById(R.id.bt_sign_out);
+        btResendOtp.setOnClickListener(this);
+        btVerifyOtp.setOnClickListener(this);
+        btSendOtp.setOnClickListener(this);
+        btSignOut.setOnClickListener(this);
+    }
+
+    void initFireBaseCallbacks(){
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                Toast.makeText(DataActivity.this, "Verification Complete", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(DataActivity.this, "Verification Failed"+e, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                Toast.makeText(DataActivity.this, "Code Sent", Toast.LENGTH_SHORT).show();
+                mVerificationId = s;
+                mResendToken = forceResendingToken;
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_send_otp:
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        etPhone.getText().toString(),        // Phone number to verify
+                        1,                 // Timeout duration
+                        TimeUnit.MINUTES,   // Unit of timeout
+                        this,               // Activity (for callback binding)
+                        mCallbacks);        // OnVerificationStateChangedCallbacks
+                break;
+            case R.id.bt_resend_otp:
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        etPhone.getText().toString(),        // Phone number to verify
+                        1  ,               // Timeout duration
+                        TimeUnit.MINUTES,   // Unit of timeout
+                        this,               // Activity (for callback binding)
+                        mCallbacks,         // OnVerificationStateChangedCallbacks
+                        mResendToken);             // Force Resending Token from callbacks
+                break;
+            case R.id.bt_verify_otp:
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, etOtp.getText().toString());
+                mAuth.signInWithCredential(credential)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = task.getResult().getUser();
+                                    Toast.makeText(DataActivity.this, "Verification Success", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                        Toast.makeText(DataActivity.this, "Verification Failed, Invalid credentials", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+                break;
+            case R.id.bt_sign_out:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(DataActivity.this, "Signing out ", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+}
---
+++ FIrebaseOtp/app/src/main/java/com/jasjotsingh/firebaseotp/MainActivity.java
@@ -0,0 +1,110 @@
+package com.jasjotsingh.firebaseotp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+//    public static final String TAG = "MainActivity";
+//    //Instance of Firebase Authentication
+//    private FirebaseAuth mAuth;
+//    // Write a message to the database
+//    FirebaseDatabase database = FirebaseDatabase.getInstance();
+//    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+//    String phoneNumber;
+//    private boolean mVerificationInProgress = false;
+//
+//    EditText editText;
+
+    Button dataActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //editText = findViewById(R.id.phoneNumber);
+        dataActivityButton = findViewById(R.id.dataActivityButton);
+        dataActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DataActivity.class);
+                startActivity(intent);
+            }
+        });
+//        phoneNumber = editText.getText().toString();
+//        //Database
+//        final DatabaseReference myRef = database.getReference("Phone Number");
+//
+//        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber, 60, TimeUnit.SECONDS, this, mCallbacks);
+//
+//        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+//            @Override
+//            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+//
+//                Log.d(TAG, "onVerificationCompleted:" + phoneAuthCredential);
+//
+//                // [START_EXCLUDE silent]
+//                mVerificationInProgress = false;
+//                // [END_EXCLUDE]
+//
+//                signInWithPhoneAuthCredential(phoneAuthCredential);
+//            }
+//
+//            @Override
+//            public void onVerificationFailed(@NonNull FirebaseException e) {
+//
+//                Toast.makeText(MainActivity.this, "Invalid Phone or Quota Exceeded", Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//    }
+//    // [START sign_in_with_phone]
+//    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+//        mAuth.signInWithCredential(credential)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithCredential:success");
+//
+//                            FirebaseUser user = task.getResult().getUser();
+//
+//
+//                        } else {
+//                            // Sign in failed, display a message and update the UI
+//                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+//                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+//                                // The verification code entered was invalid
+//                                Toast.makeText(MainActivity.this, "Sign in failed", Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                            // [START_EXCLUDE silent]
+//
+//                        }
+//                    }
+//                });
+//    }
+//    // [END sign_in_with_phone]
+}}
